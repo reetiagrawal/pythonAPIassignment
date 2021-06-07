2353706dea522ada8f92b99ef91fcebc3f6ce6f7
reetiagrawal <78945224+reetiagrawal@users.noreply.github.com>
2021-06-07 20:07:39 +0530

    Delete crud/crud directory

--- crud/crud/api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    text = models.CharField(max_length=25)
-    #lastname = models.CharField(max_length=25)
-    #email = models.CharField(max_length=50)
